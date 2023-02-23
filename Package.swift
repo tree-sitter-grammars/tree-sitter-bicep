3369ada4c66648315ec4ce46ccbd89e55e130d97
Amaan Qureshi <amaanq12@gmail.com>
2023-02-23 04:31:14 -0500

    feat: add Swift bindings

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterBicep",
+    platforms: [.macOS(.v10_13), .iOS(.v11)],
+    products: [
+        .library(name: "TreeSitterBicep", targets: ["TreeSitterBicep"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TreeSitterBicep",
+                path: ".",
+                exclude: [
+                    "binding.gyp",
+                    "bindings",
+                    "Cargo.toml",
+                    "examples",
+                    "test",
+                    "grammar.js",
+                    "LICENSE",
+                    "package.json",
+                    "README.md",
+                    "script",
+                    "src/grammar.json",
+                    "src/node-types.json",
+                ],
+                sources: [
+                    "src/parser.c",
+                ],
+                resources: [
+                    .copy("queries")
+                ],
+                publicHeadersPath: "bindings/swift",
+                cSettings: [.headerSearchPath("src")])
+    ]
+)
