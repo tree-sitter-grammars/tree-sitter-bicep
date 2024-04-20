75d55dbe404027610db4729378a41d7d3acff115
Christian Clason <c.clason@uni-graz.at>
2024-04-20 13:04:54 +0200

    fix(grammar): adapt to upstream regex change

--- grammar.js
+++ grammar.js
@@ -446,7 +446,7 @@ module.exports = grammar({
         /[0-7]{1,3}/,
         /x[0-9a-fA-F]{2}/,
         /u[0-9a-fA-F]{4}/,
-        /u{[0-9a-fA-F]+}/,
+        /u\{[0-9a-fA-F]+\}/,
       ))),
 
     interpolation: $ => prec(1, seq('${', $.expression, '}')),
