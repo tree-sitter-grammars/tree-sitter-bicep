e8dd8758f5ec25d3d851949c80f8d0e4b3a016c7
Amaan Qureshi <amaanq12@gmail.com>
2024-12-21 22:02:51 -0500

    build: update bindings

---
+++ bindings/swift/TreeSitterBicep/bicep.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_BICEP_H_
+#define TREE_SITTER_BICEP_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_bicep(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_BICEP_H_
